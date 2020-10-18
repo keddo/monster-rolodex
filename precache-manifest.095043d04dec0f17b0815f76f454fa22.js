823a97aa105371001b2fd4bec512912ecb6f595f
keddo <adu.kedir.abdurahman@gmail.com>
2020-10-18 17:00:14 +0300

    Updates

---
+++ precache-manifest.095043d04dec0f17b0815f76f454fa22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f71d386067e4b9c6d3ef28f471add5b5",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "e4ebb4a13e3339576795",
+    "url": "/monster-rolodex/static/css/main.e6873720.chunk.css"
+  },
+  {
+    "revision": "0630fe1a1c9359579e42",
+    "url": "/monster-rolodex/static/js/2.176a1076.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monster-rolodex/static/js/2.176a1076.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4ebb4a13e3339576795",
+    "url": "/monster-rolodex/static/js/main.0c51ddce.chunk.js"
+  },
+  {
+    "revision": "60e9188fe70493798c1c",
+    "url": "/monster-rolodex/static/js/runtime-main.7d7dc86d.js"
+  }
+]);
